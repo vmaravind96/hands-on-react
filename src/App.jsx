b2be8fd7369fcf1a87aaa0b3b0486a8f4e3576d9
Aravindakumar V M <aravind.kumar315@gmail.com>
2023-12-11 16:01:58 +0000

    My changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 function App() {
+  const name = "Star Admirers!"
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div className="container"> 
+      <article>
+        <hgroup>
+          <img src="images/group.svg" alt="StarGazers Group"/>
+          <h1>Meet the { name }</h1>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <button className="outline" onClick={()=>alert("Hi there !")}>Click Me</button>
+        </hgroup>
+      </article>
+    </div>
   )
 }
 export default App
