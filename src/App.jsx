8e189631ae58b52af76d195a5906ab5ee5c045f3
Aravindakumar V M <aravind.kumar315@gmail.com>
2023-12-18 23:13:10 +0000

    Changes 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
-import React, { Component, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ListCast from './components/ListCast';
+import Modals from './components/Modals';
 
 function App() {
-  const name = 'StarGazers'
   let [memberInfo, setMemberInfo] = useState(null);
+  const [cast, setCast] = useState(null);
+
+  async function fetchCast() {
+    const response = await fetch('cast.json');
+    setCast(await response.json());
+  }
+
+  useEffect(() => {
+    fetchCast();
+  });
+
   return (
     <div className="container">
       <hgroup>
         <img src="images/group.svg" alt="StarGazers Group" />
         <h1>Meet the Stargazers</h1>
         <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-        <ListCast onChoice={(info) => { setMemberInfo(info) }} />
-        {memberInfo &&
-          <article>
-            <hgroup>
-              <div style={{
-                display: "flex",
-                gap: '1rem'
-              }}>
-                <img style={{ width: '200px' }} src={`images/${memberInfo.slug}.svg`} alt={memberInfo.name} />
-                <hgroup>
-                  <h1>{memberInfo.name}</h1>
-                  <p>{memberInfo.bio}</p>
-                </hgroup>
-              </div>
-            </hgroup>
-          </article>
-        }
+        <ListCast 
+          onChoice={(info) => { setMemberInfo(info) }}
+          cast={cast} />
+          { memberInfo && 
+            <Modals member={memberInfo} 
+            handleClose={() => {setMemberInfo(null)}}/>
+          }
       </hgroup>
     </div>
   )
